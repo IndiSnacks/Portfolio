b3d663ae62c76c4ac6f48e3f513f82576760c708
Sahil Gathe <sahilsankur@gmail.com>
2025-07-22 14:30:57 -0400

    update: added resume

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -6,7 +6,7 @@ export default function Landing() {
       <div className="flex flex-col lg:flex-row">
         <div className="pb-6 h-70 w-full lg:w-2/3 bg-(--dark-color) mr-5">
         </div>
-        <p className="pt-4 lg:w-1/3"><span className='text-2xl'>H</span>ello my name is Sahil Gathe. I am a developer and designer who just graduated form UC San Diego with a B.S in Computer Science. I focus on crafting and programing unique experiences on the web. I like to bled my love love for making games with web we interact with everyday.
+        <p className="pt-4 lg:w-1/3"><span className='text-2xl'>I</span>'m am a developer and designer who just graduated form UC San Diego with a B.S in Computer Science. I focus on crafting and programing unique experiences on the web. I like to bled my love love for making games with web we interact with everyday.
         </p>
       </div>
     </div>
