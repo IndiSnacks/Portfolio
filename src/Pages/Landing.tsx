266bd508be1511cf07ce1cc7d7f2f6d0f23851bc
Sahil Gathe <sahilsankur@gmail.com>
2025-07-28 15:54:01 -0400

    fix: spelling

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -4,7 +4,7 @@ export default function Landing() {
       <div className=" flex-col text-start">
         <h1 className="text-2xl lg:text-3xl ">HELLO I'M </h1>
         <h1 className="text-4xl lg:text-6xl py-3 sarina-font"> Sahil Gathe </h1>
-        <p className="pt-4 text-xl"><span className='text-4xl'>I</span>'m am a developer and designer who just graduated form UC San Diego with a B.S in Computer Science. I focus on crafting and programing unique experiences on the web. I like to bled my love love for making games with web we interact with everyday.
+        <p className="pt-4 text-xl"><span className='text-4xl'>I</span>'m am a developer and designer who just graduated form UC San Diego with a B.S in Computer Science. I focus on crafting and programing unique experiences on the web. I like to bled my love for making games with web we interact with everyday.
         </p>
       </div>
       <img
