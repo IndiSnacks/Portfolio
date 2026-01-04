ded05200b272cf65db27c3af911b7eec8a3d040a
Sahil Gathe <sahilsankur@gmail.com>
2026-01-03 22:09:39 -0500

    minor fixes

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -6,9 +6,9 @@ export default function Landing() {
   return (
     <div
       id="landing"
-      className="flex flex-col md:flex-row lg:h-[65vh] mt-6 gap-8 items-center z-0"
+      className="flex flex-col md:flex-row md:h-[70vh] mt-6 gap-8 items-center z-0"
     >
-      <div className=" flex-col text-start md:w-2/3 w-full">
+      <div className=" flex-col text-start md:w-3/5 w-full">
         <h1 className="text-2xl lg:text-3xl ">HELLO I'M </h1>
         <h1 className="text-4xl lg:text-6xl py-3 sarina-font"> Sahil Gathe </h1>
         <p className="pt-4 text-xl">
@@ -19,7 +19,7 @@ export default function Landing() {
           everyday.
         </p>
       </div>
-      <div className="md:w-1/3 w-full">
+      <div className="md:w-2/5 w-full">
         <Suspense>
           <Mug />
         </Suspense>
